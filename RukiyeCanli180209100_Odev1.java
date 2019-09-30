55b33be941d90dc7129e8a13a93c2a1ea639dbf6
rukiyecanli1 <55897506+rukiyecanli1@users.noreply.github.com>
2019-09-30 14:09:48 +0300

    Add files via upload

--- RukiyeCanli180209100_Odev1.java
+++ RukiyeCanli180209100_Odev1.java
@@ -1,11 +1,28 @@
-
+import java.util.Scanner;
 public class RukiyeCanli180209100_Odev1 {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/** RUKÝYE CANLI 
+		 * 180209100
+		 * ENDÜSTRÝ MÜH. 1.ÖÐRETÝM
+		 * Ödev1: Ýki Nokta Arasý Mesafe Hesaplamasý
+		 */
+		public static void main(String[] args) {
+		int sayia,sayib,x1,y1,x2,y2;
+		Scanner okut=new Scanner(System.in);
+		System.out.println("A noktasýnýn koordinatlarýný giriniz:");
+		x1=okut.nextInt();
+		y1=okut.nextInt();
+		System.out.println("A noktasýnýn koordinatý("+x1+","+y1+")");
+		System.out.println("B noktasýnýn koordinatýlarýný giriniz:");
+		x2=okut.nextInt();
+		y2=okut.nextInt();
+		System.out.println("B noktasýnýn koordinatý("+x2+","+y2+")");
+		System.out.print("2 nokta arasý uzaklýk:"+Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+		
+		
+		
+		
+		
+		}
 
 	}
 
