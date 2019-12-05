3827390addfa3a4fb41a1bb35d518bb3e13e92a2
rukiyecanli1 <55897506+rukiyecanli1@users.noreply.github.com>
2019-12-05 20:57:53 +0300

    Add files via upload

---
+++ RukiyeCanlı_180209100_Odev11.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class RukiyeCanlý_180209100_Odev11 {
+
+	/*RUKÝYE CANLI
+	 * 180209100 1.ÖÐRETÝM
+	 * Ödev - 11 - Karþýlaþtýrma - Maðaza Indirim Hesaplama
+	 */
+	public static void main(String[] args) {
+	Scanner okut=new Scanner(System.in);
+	int basamak,x=1,N,toplam=0;
+	System.out.println("sayý?");
+   N=okut.nextInt();
+   while(N>0)
+   {basamak=N%10;
+ 
+   N=N/10;
+   }
+   System.out.println(toplam);
+   
+	
+	}
+	}
+
+
