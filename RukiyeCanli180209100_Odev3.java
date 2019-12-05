e4b772e4ca922307b3023e1711a962b8a9fc75fd
rukiyecanli1 <55897506+rukiyecanli1@users.noreply.github.com>
2019-12-05 20:52:57 +0300

    Add files via upload

---
+++ RukiyeCanli180209100_Odev3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class RukiyeCanli180209100_Odev3 {
+	/*Rukiye Canlý 180209100
+	 * Endustri Muh. 1.Öðretim
+	 */
+	public static void main(String[] args) {
+		double F,C;
+		Scanner okut=new Scanner(System.in);
+		System.out.print("Fahrenayt cinsinden sýcaklýk giriniz:");
+	F=okut.nextDouble();
+	C=(5/9*(F-32));
+	System.out.println( "Fahrenayt cinsinden verilen sýcaklýðýn Selsius cinsinden derecesi:"+C);
+
+	
+
+	}
+
+}
